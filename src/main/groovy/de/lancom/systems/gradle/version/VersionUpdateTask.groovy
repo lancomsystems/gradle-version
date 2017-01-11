79656581564e0c60964468eac0bfbbebca536999
Felix Gerdsmeier <felix.gerdsmeier@lancom.de>
2017-01-11 13:55:07 +0100

    initial project setup

---
+++ src/main/groovy/de/lancom/systems/gradle/version/VersionUpdateTask.groovy
@@ -0,0 +1,11 @@
+package de.lancom.systems.gradle.version
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class VersionUpdateTask extends DefaultTask {
+
+    @TaskAction
+    def exec() {
+    }
+}
